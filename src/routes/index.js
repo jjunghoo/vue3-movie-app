6dff5c55fa0428e944a7d8d2670186dcf57850e2
jjunghoo <wjdgn2468@naver.com>
2022-01-03 15:32:34 +0900

    Create vue project

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './Home.vue'
+import About from './About.vue'
+
+export default createRouter({
+    // Hash
+    // https://google.com/#/search
+    history: createWebHashHistory(),
+    // page
+    // https:google.com/
+    routes: [
+        {
+            path: '/',
+            component: Home
+        },
+        {
+            path: '/about',
+            component: About
+        }
+    ]
+})
