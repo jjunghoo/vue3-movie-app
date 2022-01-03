849154363508e252ae09842fa560a570034fdde8
jjunghoo <wjdgn2468@naver.com>
2022-01-03 16:50:24 +0900

    Add Movie component and active-class

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
+  <Header></Header>
   <RouterView></RouterView>
 </template>
+
+<script>
+import Header from '~/components/Header.vue'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
 
 <style lang="scss">
   @import "~/scss/main.scss";
